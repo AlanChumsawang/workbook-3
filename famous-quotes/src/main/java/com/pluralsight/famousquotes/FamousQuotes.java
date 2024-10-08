597e8dfed458c38673efb1989738656188f59c88
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-08 09:39:18 -0400

    Famous quote generator with exception handling

---
+++ famous-quotes/src/main/java/com/pluralsight/famousquotes/FamousQuotes.java
@@ -0,0 +1,46 @@
+package com.pluralsight.famousquotes;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class FamousQuotes {
+    static String[] quotes = {
+            "Stay hungry, stay foolish.",
+            "Knowledge is power.",
+            "Simplicity is the ultimate sophistication.",
+            "To be or not to be.",
+            "I think, therefore I am.",
+            "Actions speak louder than words.",
+            "Fortune favors the bold.",
+            "Live and let live.",
+            "Time is money.",
+            "Less is more."
+    };
+
+    public static void main(String[] args) {
+        Scanner inputscanner = new Scanner(System.in);
+        boolean on = true;
+        while (on) {
+            System.out.print("""
+                            \s
+                            Welcome to Quote Generator!
+                            \s
+                    Choose a number 1 - 10: 
+                    """);
+
+            try {
+                int userInput = inputscanner.nextInt();
+                inputscanner.nextLine();
+                System.out.println("\n\n" + quotes[userInput]);
+            } catch (Exception ArrayIndexOutOfBoundsException) {
+                System.out.println("\nInvalid Entry. Enter a number 1-10");
+            }
+            System.out.println("\nKeep Going? Y/N");
+            String keepGoing = inputscanner.nextLine();
+            try {
+            if (Objects.equals(keepGoing, "N") || Objects.equals(keepGoing, "n")) {
+                on = false;
+            }
+        }
+    }
+}
