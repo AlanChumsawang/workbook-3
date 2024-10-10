9dd5f0ba7da4fb392bb2be15c82359a4ebe437b2
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-10 11:39:08 -0400

    Completed Exercise 2 in Workbook 3 which formats dates

---
+++ date-formatter/src/main/java/com/pluralsight/dateformatter/FormatDates.java
@@ -0,0 +1,25 @@
+package com.pluralsight.dateformatter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class FormatDates {
+
+    public static void main(String[] args) {
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println("Today is: " + today);
+
+        DateTimeFormatter dayMDY =
+                DateTimeFormatter.ofPattern("MMM-dd-yyyy ");
+        DateTimeFormatter time =
+                DateTimeFormatter.ofPattern("HH:mm");
+
+
+        String formattedTime = today.format(time);
+        String formattedDate = today.format(dayMDY);
+        System.out.println(formattedTime + " on " + formattedDate);
+
+    }
+
+}
--- online-store/src/main/java/com/pluralsight/onlinestore/Store.java
+++ online-store/src/main/java/com/pluralsight/onlinestore/Store.java
@@ -3,6 +3,7 @@ package com.pluralsight.onlinestore;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Store {
@@ -31,25 +32,30 @@ public class Store {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
 
-    public void displayInventory() {
+    public void displayInventory(Scanner scanner) {
         for (Products product : storeInventory) {
             System.out.println(product.getProductName());
             System.out.println("    " + "SKU: " + product.getSku());
             System.out.printf("    " + "$ " + product.getPrice() + "\n");
             System.out.println("    " + "Department: " + product.getDepartment() + "\n");
         }
+        System.out.println("Would you like to add a product to your cart? (y/n)");
+        String userChoice = scanner.nextLine();
+        if (userChoice.equalsIgnoreCase("y") ){
+            addProductToCart(scanner);
+        }
+        el
     }
 
     public void checkout() {
         System.out.println("Checking out");
     }
 
-    public void displayCart(ArrayList<Products> cart) {
+    public void displayCart(Scanner scanner) {
         System.out.println("Your cart:\n");
-        if (cart == null) {
+        if (cart.isEmpty()) {
             System.out.println("Your cart is empty");
         } else {
             for (Products product : cart) {
@@ -57,24 +63,23 @@ public class Store {
                 System.out.println("    " + "SKU: " + product.getSku());
                 System.out.printf("    " + "$" + product.getPrice() + "\n");
                 System.out.println("    " + "Department: " + product.getDepartment() + "\n");
-                System.out.println("Total: $ " + calculateTotal(cart));
-                System.out.println("\n1: Checkout\n2: Remove product\n3: Exit");
-                Scanner scanner = new Scanner(System.in);
-                int choice = scanner.nextInt();
-                scanner.nextLine();
-                switch (choice) {
-                    case 1:
-                        System.out.println("Checking out");
-                        break;
-                    case 2:
-                        System.out.println("Enter the SKU of the product you would like to remove: ");
-                        String productSku = scanner.nextLine().trim().toUpperCase();
-                        removeProduct(productSku);
-                        break;
-                    case 3:
-                        System.out.println("Exiting");
-                        break;
-                }
+            }
+            System.out.println("Total: $ " + calculateTotal(cart));
+            System.out.println("\n1: Checkout\n2: Remove product\n3: Exit");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("Checking out");
+                    break;
+                case 2:
+                    System.out.println("Enter the SKU of the product you would like to remove: ");
+                    String productSku = scanner.nextLine().trim().toUpperCase();
+                    removeProduct(productSku);
+                    break;
+                case 3:
+                    System.out.println("Exiting");
+                    break;
             }
         }
     }
@@ -87,25 +92,41 @@ public class Store {
         return total;
     }
 
-    public void addProductToCart(Products product) {
-        cart.add(product);
+    public void addProductToCart(Scanner scanner) {
+        boolean continueShopping = true;
+        while (continueShopping) {
+            System.out.println("Enter the sku of the product you would like to add to your cart: ");
+            String productSku = scanner.nextLine();
+            for (Products items : storeInventory) {
+                if (items.getSku().equalsIgnoreCase(productSku)) {
+                    cart.add(items);
+                    System.out.println("Product added to cart");
+                    System.out.println("Continue shopping? (y/n)");
+                    char continueShoppingChoice = scanner.next().charAt(0);
+                    scanner.nextLine();
+                    if (continueShoppingChoice == 'n') {
+                        continueShopping = false;
+                    }
+                }
+            }
+        }
+
     }
 
     public void removeProduct(String productSku) {
         for (Products product : cart) {
             if (product.getSku().equals(productSku)) {
-                storeInventory.remove(product);
+                cart.remove(product);
                 System.out.println("Product removed");
-            } else {
-                System.out.println("Product not found");
+                return;
             }
         }
+        System.out.println("Product not found");
     }
 
-    public void displayMenu() {
-        Scanner scanner = new Scanner(System.in);
+    public void displayMenu(Scanner scanner) {
         System.out.print("""
-                
+
                                           ████████╗██╗  ██╗███████╗                      \s
                                           ╚══██╔══╝██║  ██║██╔════╝                      \s
                                              ██║   ███████║█████╗                        \s
@@ -124,49 +145,20 @@ public class Store {
                                           ╚════██║   ██║   ██║   ██║██╔══██╗██╔══╝       \s
                                           ███████║   ██║   ╚██████╔╝██║  ██║███████╗     \s
                                           ╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝     \s     \s
-                
+
                 1: Display Inventory
                 2: Display Cart
                 3: Exit
-                
+
                 Choose an option: """);
         int choice = scanner.nextInt();
         scanner.nextLine();
         switch (choice) {
             case 1:
-                displayInventory();
-                boolean continueShopping = true;
-                while (continueShopping) {
-                    System.out.println("Would you like to add a product to your cart? (y/n)");
-
-                    char userChoice = scanner.next().charAt(0);
-                    scanner.nextLine();
-                    if (userChoice == 'y') {
-                        System.out.println("Enter the sku of the product you would like to add to your cart: ");
-                        String productSku = scanner.nextLine();
-                        for (Products product : storeInventory) {
-                            if (product.getSku().equalsIgnoreCase(productSku)) {
-                                addProductToCart(product);
-                                System.out.println("Product added to cart");
-                                System.out.println("Continue shopping? (y/n)");
-                                char continueShoppingChoice = scanner.next().charAt(0);
-                                scanner.nextLine();
-                                if (continueShoppingChoice == 'n') {
-                                    continueShopping = false;
-                                }
-                            }
-                        }
-                    } else if (userChoice == 'n') {
-                        System.out.println("Exiting");
-                        continueShopping = false;
-                    } else {
-                        System.out.println("Enter 'y' or 'n'");
-                    }
-                }
-                    break;
-
+                displayInventory(scanner);
+                break;
             case 2:
-                displayCart(cart);
+                displayCart(scanner);
                 break;
             case 3:
                 System.out.println("Exiting");
@@ -174,14 +166,10 @@ public class Store {
         }
     }
 
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Store store = new Store(new ArrayList<Products>());
         store.getInventory();
-        store.displayMenu();
-
-
-        //store.displayInventory();
+        store.displayMenu(scanner);
     }
 }
