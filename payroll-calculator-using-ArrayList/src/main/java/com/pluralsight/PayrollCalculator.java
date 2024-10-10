917928a50513e2e7b49f05dcd42b7d040934fc03
AlanChumsawang <alan.chumsawang@outlook.com>
2024-10-10 09:25:44 -0400

    Added writeToFile method

--- payroll-calculator-using-ArrayList/src/main/java/com/pluralsight/PayrollCalculator.java
+++ payroll-calculator-using-ArrayList/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,12 +1,11 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PayrollCalculator {
-    static ArrayList<Employees> employeesList = new ArrayList<>();
+    private static final ArrayList<Employees> employeesList = new ArrayList<>();
 
     public static void searchEmployee() {
         Scanner inputScanner = new Scanner(System.in);
@@ -24,10 +23,18 @@ public class PayrollCalculator {
             }
         }
     }
+    public static void printEmployeeData() {
+        for (Employees employee : employeesList) {
+            System.out.println("Employee ID: " + employee.getEmployeeID());
+            System.out.println("Employee Name: " + employee.getName(employee));
+            System.out.println("Pay Rate: " + employee.getPayRate(employee));
+            System.out.println("Total Pay: " + employee.calculatePay(employee) + "\n");
+        }
+    }
 
-    public static void getEmployeeData() {
+    public static void getEmployeeData(String fileName) {
         try {
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            FileReader fileReader = new FileReader(fileName);
             BufferedReader bufReader = new BufferedReader(fileReader);
             String input = bufReader.readLine();
 
@@ -40,35 +47,72 @@ public class PayrollCalculator {
                 employeesList.add(new Employees(employeeID, name, payRate, hoursWorked));
             }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            Scanner inputScanner = new Scanner(System.in);
+            System.out.println("\n\n\nFile not found\n\n\nEnter the name of the file employee file to process:");
+            fileName = inputScanner.nextLine();
+            getEmployeeData("src/main/resources/" + fileName);
         }
 
     }
 
+    public static void writeEmployeeData(String newFileName) {
+        try{
+            FileWriter fileWriter = new FileWriter(newFileName);
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            bufWriter.write("id|name|gross pay\n");
+            for (Employees employee : employeesList) {
+                bufWriter.write(employee.getEmployeeID() + "|" + employee.getName(employee) + "|" + employee.getHoursWorked(employee) + "|" + employee.getPayRate(employee) + "\n");
+            }
+            bufWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        getEmployeeData();
+        System.out.println("Welcome to the Payroll Calculator!");
         boolean continueMenu = true;
         while (continueMenu) {
             System.out.print("""
-                    Welcome to the Payroll Calculator!
                     
                     1: Search Employee by ID
-                    2: Exit
+                    2: Display all Employee Data
+                    3: Create a new payroll file
+                    4: Exit
                     
                     
                     Choose an option: """);
             int choice = inputScanner.nextInt();
             inputScanner.nextLine();
 
+            String filePath = "src/main/resources/";
+            if (choice != 4) {
+                System.out.println("\nEnter the name of the file employee file to process:");
+                String fileName = inputScanner.nextLine();
+                getEmployeeData(filePath + fileName);
+            }
+
             switch (choice) {
                 case 1:
                     searchEmployee();
                     break;
                 case 2:
-                    System.out.println("Exiting");
+                    printEmployeeData();
+                    break;
+                case 3:
+                    System.out.print("\nEnter the name of the payroll file to create: ");
+                    String newFileName = inputScanner.nextLine();
+                    writeEmployeeData(filePath + newFileName);
+                    break;
+                case 4:
+                    System.out.println("\nExiting");
                     continueMenu = false;
                     break;
+                default:
+                    System.out.println("Invalid choice. Please try again. \n\n");
             }
         }
     }
